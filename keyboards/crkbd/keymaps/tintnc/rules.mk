9c51360fe3db1895bf753f26d3d25e9c7425aeae
Tin TA <tinta@Binbins-MBP.lan>
2024-12-16 01:03:18 +0700

    add custom bongo cat and keymaps

---
+++ keyboards/crkbd/keymaps/tintnc/rules.mk
@@ -0,0 +1,9 @@
+CONSOLE_ENABLE = no          # Save ~400 bytes
+COMMAND_ENABLE = no         # Save ~200 bytes
+MOUSEKEY_ENABLE = no        # Save ~500-1000 bytes
+OLED_ENABLE = yes           # OLED takes space but you probably want to keep it
+
+# Extra features
+EXTRAKEY_ENABLE = yes  # For media keys
+LTO_ENABLE = yes      # Link Time Optimization, reduces compiled size
+WPM_ENABLE = yes      # Words per minute counter
